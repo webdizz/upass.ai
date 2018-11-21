252a0bfe0c589e35a6d5c9eb967f9c7fcfa7bf08
Izzet Mustafaiev <izzet@mustafaiev.com>
2018-11-21 14:21:13 +0200

    Increased flexibility of model creation based on pre-trained models

--- upassai/models.py
+++ upassai/models.py
@@ -8,25 +8,14 @@ __all__ = ['SiameseNetwork', 'contrastive_loss']
 
 class SiameseNetwork(nn.Module):
 
-    def __init__(self):
+    def __init__(self, architecture: nn.Module = models.resnet50, pretrained: bool = True, cut: int = None, nf: int = 1000, nc: int = 100, lin_ftrs: Optional[Collection[int]] = None, ps: Floats = 0.5):
         super().__init__()
-        # self.cnn1 = create_body(torchvision.models.densenet121(True))
-        self.cnn1 = create_body(models.resnet50(pretrained=True))
-        self.fc1 = nn.Sequential(
-            nn.Linear(1000, 500),
-            nn.ReLU(inplace=True),
-
-            nn.Linear(500, 500),
-            nn.ReLU(inplace=True),
-
-            nn.Linear(500, 5)
-        )
+        self.body = create_body(architecture(pretrained=pretrained), cut=cut)
+        self.head = create_head(nf, nc, lin_ftrs, ps)
 
     def forward_once(self, x):
-        x = self.cnn1(x)
-        x = F.relu(x)
-        x = self.fc1(x)
-
+        x = self.body(x)
+        x = self.head(x)
         return x
 
     def forward(self, input1, input2):
@@ -36,12 +25,17 @@ class SiameseNetwork(nn.Module):
 
 
 def contrastive_loss(outputs, label,  margin=2.0):
+    """
+    Contrastive loss function.
+    Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+    """
     output1, output2 = outputs
     euclidean_distance = F.pairwise_distance(output1, output2)
     label = torch.from_numpy(label.cpu().data.numpy()).to(torch.float).cpu()
 
     loss_contrastive = torch.mean(
         (1-label) * torch.pow(euclidean_distance, 2).cpu()
-        + (label) * torch.pow(torch.clamp(margin - euclidean_distance, min=0.0), 2).cpu()
+        + (label) * torch.pow(torch.clamp(margin -
+                                          euclidean_distance, min=0.0), 2).cpu()
     )
     return loss_contrastive
